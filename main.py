6547b6fdc357b9ebfd92b947edf0dc25ddf7b55b
pcote <cotejrp@gmail.com>
2015-08-19 10:35:53 -0400

    Can now respond to power and toughness queries on the actual page

--- main.py
+++ main.py
@@ -9,10 +9,32 @@ def index():
 
 @app.route("/lookupcards", methods=["GET"])
 def get_info():
-    results = [
-        dict(name="Applejack", type="Pony Creature", rarity="Common", artist="Lauren Faust", set_name="Magic 2015")
-    ]
-    return jsonify({"results":results})
+    base_url = "http://104.236.34.0/"
+
+    def _reqargs(arg):
+        return request.args.get(arg)
+    # strength
+    power = _reqargs("power")
+    toughness = _reqargs("toughness")
+    color = _reqargs("color")
+    loyalty = _reqargs("loyalty")
+
+    if power or toughness:
+        strength_arg = "strength?"
+        if power:
+            strength_arg = strength_arg + "power=" + power + "&"
+        if toughness:
+            strength_arg = strength_arg + "toughness=" + toughness
+
+        strength_arg = strength_arg.rstrip("&")
+        full_url = base_url + strength_arg
+        print(full_url)
+        strength_results = requests.get(full_url).json().get("results")
+
+
+    card_list = strength_results
+    print(card_list)
+    return jsonify({"results":card_list})
 
 if __name__ == "__main__":
     app.run(debug=True)
