e5073e83914a812ddf076287a509448e4a787f9b
pcote <cotejrp@gmail.com>
2015-08-20 18:16:14 -0400

    Created a card collection class to help handle elimination of cards that don't meet ALL stated requirements submitted by user

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, redirect, request, jsonify
 import requests
 
+class CardCollection(object):
+    def __init__(self):
+        self._queue = []
+
+    def update_queue(self, new_data):
+        if len(self._queue) == 0:
+            self._queue = new_data
+            return
+
+        self._queue = [card for card in self._queue
+                        if card.get("name") in [card.get("name") for card in new_data]]
+
+    @property
+    def collection(self):
+        return self._queue
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,6 +35,7 @@ def get_info():
     color = _reqargs("color")
     loyalty = _reqargs("loyalty")
 
+    card_collection = CardCollection()
     strength_results = []
     if power or toughness:
         strength_arg = "strength?"
@@ -31,11 +48,23 @@ def get_info():
         full_url = base_url + strength_arg
         print(full_url)
         strength_results = requests.get(full_url).json().get("results")
+        card_collection.update_queue(strength_results)
+
+    if color:
+        full_url = base_url + "color?color=" + color
+        results = requests.get(full_url)
+        results = requests.get(full_url).json().get("results")
+        card_collection.update_queue(results)
+
+    if loyalty:
+        full_url = base_url + "loyalty/" + loyalty
+        results = requests.get(full_url)
+        results = requests.get(full_url).json().get("results")
+        card_collection.update_queue(results)
 
 
-    card_list = strength_results
-    print(card_list)
-    return jsonify({"results":card_list})
+    print(card_collection.collection)
+    return jsonify({"results":card_collection.collection})
 
 if __name__ == "__main__":
     app.run(debug=True)
